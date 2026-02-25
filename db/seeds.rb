4af80a7965011d390f73371359e4c497ec22b778
Xavier Pardoue <pardoue.xavier@gmail.com>
2026-02-25 13:00:32 +0100

    finis enfin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+Restaurant.destroy_all
+puts "Les restos sont bien détruits"
+resto_category = [ "chinese", "italian", "japanese", "french", "belgian" ]
+
+6.times do
+  Restaurant.create!(name: Faker::Restaurant.name,
+  address: Faker::JapaneseMedia::Doraemon.location,
+  category: resto_category.sample,
+  phone_number: Faker::PhoneNumber.cell_phone_in_e164)
+end
+puts "Création des restos terminée, all good"
+
+25.times do
+  Review.create!(rating: [ 0, 1, 2, 3, 4, 5 ].sample,
+  content: Faker::Restaurant.review,
+  restaurant: Restaurant.all.sample)
+end
+puts "Création des avis terminée!"
